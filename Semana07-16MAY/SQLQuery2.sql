32a7a416ef620438668f22f8ef0d319d77a2f77d
Gustavo Coronel <gcoronelc@gmail.com>
2024-05-15 11:49:23 -0500

    Semana 7

--- Semana07-16MAY/SQLQuery2.sql
+++ Semana07-16MAY/SQLQuery2.sql
@@ -42,6 +42,79 @@ end;
 go
 
 
+alter table productos 
+add constraint pk_productos
+primary key(productid);
+go
+
+
+select * from productos
+where ProductID=1000000;
+go
+
+
+select * 
+from Customers c
+join Orders o on c.CustomerID = o.CustomerID;
+go
+
+
+select * 
+from Orders o
+join Customers c on o.CustomerID = c.CustomerID;
+go
+
+-- subconsulta tipo tabla
+
+select top 1 ProductID, sum(Quantity) cant
+from [Order Details] d
+group by ProductID
+order by 2 desc
+
+
+select * 
+from productos p
+join (select top 1 ProductID, sum(Quantity) cant
+	from [Order Details] d
+	group by ProductID
+	order by 2 desc
+) t on p.ProductID = t.ProductID;
+go
+
+
+-- subconsulta recursiva
+
+select x.ProductID 
+from (select top 1 d.ProductID, sum(Quantity) cant
+from [Order Details] d
+join Products p on d.ProductID = p.ProductID
+where p.CategoryID = 1
+group by d.ProductID
+order by 2 desc
+) x
+
+
+
+select * 
+from Products pr
+where pr.ProductID = (
+	select x.ProductID 
+	from (select top 1 d.ProductID, sum(Quantity) cant
+	from [Order Details] d
+	join Products p on d.ProductID = p.ProductID
+	where p.CategoryID = pr.CategoryID
+	group by d.ProductID
+	order by 2 desc
+	) x
+);
+go
+
+
+
+
+
+
+
 
 
 
